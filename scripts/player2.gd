2809785f6ba27497294cab4084f4dd042ca9e39f
loens_g <gregoire.loens@epitech.eu>
2018-01-27 16:51:40 +0100

    merge

--- scripts/player2.gd
+++ scripts/player2.gd
@@ -8,26 +8,26 @@ var guard  = false
 func _process(delta):
 	timer += delta
 	if pressButton == false:
-		if Input.is_action_pressed("player1_guard"):
+		if Input.is_action_pressed("player2_guard"):
 			guard = true
-			set_texture(load("res://sprites/adon_guard.png"))
-		if Input.is_action_pressed("player1_punch"):
+			set_texture(load("res://sprites/joe_guard.png"))
+		if Input.is_action_pressed("player2_punch"):
 			guard = false
 			pressButton = true
 			resetTime = 0.2
 			timer = 0
-			set_texture(load("res://sprites/adon_jab.png"))
-		if Input.is_action_pressed("player1_kick"):
+			set_texture(load("res://sprites/joe_jab.png"))
+		if Input.is_action_pressed("player2_kick"):
 			guard = false
 			pressButton = true
 			resetTime = 0.4
 			timer = 0
-			set_texture(load("res://sprites/adon_kick.png"))
+			set_texture(load("res://sprites/joe_kick.png"))
 	if (timer > resetTime && guard == false):
 		pressButton = false
 		resetTime = 0
 		timer = 0
-		set_texture(load("res://sprites/adon_stand.png"))
+		set_texture(load("res://sprites/joe_stand.png"))
 
 
 func _ready():
