addfb54e4ea07b636abab83cd8d6ad61453cc4a8
Jeremy Thoveron <jeremy.thoveron-@epitech.eu>
2018-01-27 16:26:26 +0100

    Clean repo

---
+++ scripts/Menu.gd
@@ -0,0 +1,18 @@
+extends Panel
+
+func _ready():
+	pass
+
+
+func _on_HistoireButton_pressed():
+	var scene = load("res://scenes//splash.tscn")
+	var node = scene.instance()
+	add_child(node)
+	pass
+
+
+func _on_VersusButton_pressed():
+	var scene = load("res://scenes//splash.tscn")
+	var node = scene.instance()
+	add_child(node)
+	pass
