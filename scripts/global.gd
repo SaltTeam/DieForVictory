941bc21c7cc7f42f09d50defef3d5577f36aa5d9
scorsi <sylvain.corsini@gmail.com>
2018-01-27 18:10:35 +0100

    Game works

--- scripts/global.gd
+++ scripts/global.gd
@@ -1,6 +1,7 @@
 extends Node
 
 export var experience = 0
+export var next_level = ""
 
 const EXPERIENCE_PER_WIN = 10
 export var nb_wins = 0
@@ -10,12 +11,14 @@ export var nb_looses = 0
 # LIFE
 const LIFE_COST = 5
 const BASE_LIFE = 25
-export var life = BASE_LIFE
+export var life = 0
+export var max_life = BASE_LIFE
 
 # GUARD
 const GUARD_CAPACITY_COST = 5
 const BASE_GUARD_CAPACITY = 50
-export var guard_capacity = BASE_GUARD_CAPACITY
+export var guard_capacity = 0
+export var max_guard_capacity = BASE_GUARD_CAPACITY
 
 const GUARD_TIME_COST = 10
 const BASE_GUARD_TIME = 2
@@ -38,3 +41,12 @@ export var kick_damage = BASE_KICK_DAMAGE
 const KICK_TIME_COST = 15
 const BASE_KICK_TIME = 0.4
 export var kick_time = BASE_KICK_TIME
+
+func init_global():
+	life = max_life
+	guard_capacity = max_guard_capacity
+
+func end_level():
+	var scene = load("res://scenes/ContinueScreen.scn")
+	var node = scene.instance()
+	get_tree().add_child(node)
