1ed1a2b88b9e682338ac9388fccb977e78057f46
pillon_m <maxime.pillon@epitech.eu>
2018-01-28 10:33:05 +0100

    adding new sound

--- scripts/SplashScreenMusic.gd
+++ scripts/SplashScreenMusic.gd
@@ -1,7 +1,4 @@
 extends SamplePlayer
 
 func _ready():
-	var sample = get_node("music").get_sample_library().get_sample("OOaih")
-	#play("OOaih")
-	sample.set_loop_format(sample.LOOP_FORWARD)
-    sample.set_loop_begin(0)
+	play("OOaih")
